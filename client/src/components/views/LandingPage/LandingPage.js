796543f598ce185656df8cca223106bbd00e6d0a
jiho <wjeeho@gmail.com>
2022-05-18 07:27:02 +0900

    프록시 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios';
 
 function LandingPage() {
+
+  useEffect(()=>{
+    axios.get('/api/hello')
+    .then(res => console.log(res))
+  },[])
+
   return (
     <div>LandingPage 랜딩 페이지 왜 안보임</div>
   )
