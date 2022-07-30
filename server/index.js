6033100a1b85fbabd0ec4e697987f3154e88f8d1
jiho <wjeeho@gmail.com>
2022-07-30 16:25:10 +0900

    fix index.js

--- server/index.js
+++ server/index.js
@@ -28,15 +28,24 @@ mongoose.connect(config.mongoURI , {
 
 
 
-if(process.env.NODE_ENV === "production") {
-    // app.use(cors());
-    app.use(express.static("client/build"));
+// if(process.env.NODE_ENV === "production") {
+//     // app.use(cors());
+//     app.use(express.static("client/build"));
 
-    app.get("*", (req, res) =>{
-        res.sendFile(path.resolve(__dirname, "../client","build","index.html"));
+//     app.get("*", (req, res) =>{
+//         res.sendFile(path.resolve(__dirname, "../client","build","index.html"));
+//     });
+// }
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static(path.join(__dirname, "/../client/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "/../client/build", "index.html"));
     });
-}
-
+  }
+ 
 
 app.get('/', (req,res)=> res.send('Hello World 그럼 여기서만 바꾸면 된다는거지? 이거는 live그거랑은 연동이 안되네'))
 
