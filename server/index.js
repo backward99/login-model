5c28d96cd05af040bb54162d06096d28b1bb8a77
jiho <wjeeho@gmail.com>
2022-07-30 15:41:59 +0900

    fix index.js

--- server/index.js
+++ server/index.js
@@ -29,8 +29,8 @@ mongoose.connect(config.mongoURI , {
 
 
 if(process.env.NODE_ENV === "production") {
-    app.use(cors());
-    // app.use(express.static("client/build"));
+    // app.use(cors());
+    app.use(express.static("client/build"));
 
     app.get("*", (req, res) =>{
         res.sendFile(path.resolve(__dirname, "../client","build","index.html"));
