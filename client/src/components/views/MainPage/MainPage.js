f2dd0f5710d72f41fdba85e99d73ef0a66c8eee0
jiho <wjeeho@gmail.com>
2022-07-30 15:29:01 +0900

    fix

--- client/src/components/views/MainPage/MainPage.js
+++ client/src/components/views/MainPage/MainPage.js
@@ -11,41 +11,6 @@ import axios from 'axios';
 import moment from 'moment';
 
 function MainPage() {
-    const [Json, setJson] = useState([]);
-    useEffect(() => {
-        axios.get('api/json/getJsons')
-            .then(response => {
-                if (response.data.success) {
-                    console.log(response.data);
-                    setJson(response.data.jsons)
-                } else {
-                    alert('json 가져오기를 실패했습니다.');
-                }
-            })
-
-    }, [])
-    const renderCards = Json.map((json, index) => {
-        console.log('json:', json);
-        return <Col key={index} lg={6} md={8} xs={24}>
-            <a href={`/json/post/${json._id}`} >
-                <div style={{ position: 'relative' }}>
-                </div>
-            </a>
-            <br />
-            <Meta
-            // title={json.title}
-            // description=""
-            >
-            </Meta>
-            <div> 제목 : {json.title}</div>
-            <div> 파일 이름 : {json.realName}</div>
-            <div> 작성자 : {json.writer.name}</div>
-
-
-
-            <div>날짜 :  {moment(json.createdAt).format("MMM Do YY")} </div>
-        </Col>
-    })
     return (
         <div>
             로그인이 필요합니다.
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser'); 
 const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
@@ -25,13 +25,16 @@ mongoose.connect(config.mongoURI , {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(()=>console.log('mongoDB connect'))
 .catch(err=>console.log(err))
-//이렇게
-//이렇게
-//이렇게
-//이렇게
-//이렇게
 
 
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) =>{
+        res.sendFile(path.resolve(__dirname, "../client","build","index.html"));
+    });
+}
 
 
 app.get('/', (req,res)=> res.send('Hello World 그럼 여기서만 바꾸면 된다는거지? 이거는 live그거랑은 연동이 안되네'))
@@ -111,12 +114,5 @@ app.get('/api/users/logout', auth, (req,res)=>{
 
 
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-
-    app.get("*", (req, res) =>{
-        res.sendFile(path.resolve(__dirname, "../client","build","index.html"));
-    });
-}
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
