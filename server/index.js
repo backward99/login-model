5234e2cda5752251453ad99d3de570c6828f48ba
jiho <wjeeho@gmail.com>
2022-07-31 13:58:59 +0900

    please

--- server/index.js
+++ server/index.js
@@ -37,14 +37,7 @@ mongoose.connect(config.mongoURI , {
 //     });
 // }
 
-if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static(path.join(__dirname, "../client/build")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-    });
-  }
  
 
 // app.get('/', (req,res)=> res.send('Hello World 그럼 여기서만 바꾸면 된다는거지? 이거는 live그거랑은 연동이 안되네'))
@@ -122,7 +115,14 @@ app.get('/api/users/logout', auth, (req,res)=>{
 
 
 
+if (process.env.NODE_ENV === "production") {
 
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+    });
+  }
 
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
